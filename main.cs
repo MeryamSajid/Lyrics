e635814b6855e1a5be772bb91a1e68490b213369
MeryamSajid <143551946+MeryamSajid@users.noreply.github.com>
2023-09-03 02:46:01 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  
+    string line1;
+    string line2;
+    string line3;
+    string line4;
+    string line5;
+
+    Console.Write ("Enter first line of lyrics: ");
+    line1 = Console.ReadLine();
+
+    Console.Write ("Enter second line of lyrics: ");
+    line2 = Console.ReadLine();
+
+    Console.Write ("Enter third line of lyrics: ");
+    line3 = Console.ReadLine();
+    
+    Console.Write ("Enter fourth line of lyrics: ");
+    line4 = Console.ReadLine(); 
+
+    Console.Write ("Enter fifth line of lyrics: ");
+    line5 = Console.ReadLine(); 
+
+    Console.WriteLine(line1);
+    Console.WriteLine(line2);
+    Console.WriteLine(line3);
+    Console.WriteLine(line4);
+    Console.WriteLine(line5);
+}
+
+};
+  
